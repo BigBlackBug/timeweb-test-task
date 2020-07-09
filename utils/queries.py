d15e7d6cefc70c5b4178f308d7c2295f0906c86b
bigblackbug <bigblackbugg@gmail.com>
2020-07-09 19:46:16 +0300

    style(queries): make all queries upcase
    Because it's more stylish

--- utils/queries.py
+++ utils/queries.py
@@ -1,22 +1,25 @@
 CREATE_TABLES = """ 
-CREATE TABLE if not exists directory(
-    id integer PRIMARY KEY, 
-    parent_dir_id integer REFERENCES directory(id),
-    dir_path text NOT NULL
+CREATE TABLE IF NOT EXISTS directory(
+    id INTEGER PRIMARY KEY, 
+    parent_dir_id INTEGER REFERENCES directory(id),
+    dir_path TEXT NOT NULL
 );
 CREATE UNIQUE INDEX IF NOT EXISTS directory_unique_path_index 
 ON directory(dir_path);
 CREATE INDEX IF NOT EXISTS directory_parent_id_index 
 ON directory(parent_dir_id);
-CREATE TABLE if not exists file(
-    id integer PRIMARY KEY, 
-    dir_id integer REFERENCES directory(id) ON DELETE CASCADE,
-    filename text NOT NULL,
-    last_modified timestamp NOT NULL,
-    permissions integer NOT NULL,
+CREATE TABLE IF NOT EXISTS file(
+    id INTEGER PRIMARY KEY, 
+    dir_id INTEGER REFERENCES directory(id) ON DELETE CASCADE,
+    filename TEXT NOT NULL,
+    last_modified TIMESTAMP NOT NULL,
+    permissions INTEGER NOT NULL,
     sha256 char(64) NOT NULL
 );"""
 
-DROP_TABLES = "DROP table if exists file; DROP table if exists directory"
+DROP_TABLES = "DROP TABLE IF EXISTS file;" \
+              "DROP TABLE IF EXISTS directory;" \
+              "DROP INDEX IF EXISTS directory_unique_path_index;" \
+              "DROP INDEX IF EXISTS directory_parent_id_index;"
 
 ENABLE_FK = "PRAGMA foreign_keys = ON;"
